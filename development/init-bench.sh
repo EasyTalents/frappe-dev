63dc09f18f11afda7a430d73213d98d4264ad810
Jeffreyapi <63817157+Jeffreyapi@users.noreply.github.com>
2025-11-15 12:59:26 +0000

    fix folder init-bench.sh

--- development/init-bench.sh
+++ development/init-bench.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-# On travaille depuis la racine du repo cloné dans le container
-cd /workspace
+# On travaille depuis le dossier development du repo
+cd /workspace/development
 
 # Si le bench existe déjà, on ne refait rien (idempotent)
-if [ -d "development/frappe-bench" ]; then
+if [ -d "frappe-bench" ]; then
   echo "[init-bench] frappe-bench déjà présent, on ne fait rien."
   exit 0
 fi
@@ -15,14 +15,14 @@ echo "[init-bench] Création du bench et du site de dev..."
 # Utilise l'installeur officiel qui automatise :
 # - création bench
 # - création site
-# - installation des apps de apps-example.json
+# - installation des apps de apps-example.json (par défaut)
 #  --apps-json apps-example.json \
 python installer.py \
   --bench-name frappe-bench \
   --site-name development.localhost \
   --frappe-branch version-15
 
-cd development/frappe-bench
+cd frappe-bench
 
 # Active le mode développeur sur le site
 bench --site development.localhost set-config developer_mode 1
